e942afa8a71ec8deabc022c5c6f40331ca5341d2
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-03-18 22:24:46 +0300

    PDO

--- setting.php
+++ setting.php
@@ -7,10 +7,14 @@ $pass = '';
 try{
 // PDO class
 $db = new  PDO($dsn,$user,$pass);
-// PDO Has attrabutes 
-// Has Drivers
-// has oppetion you can modify it
+// PDO Has attrabutes // Has Drivers// has oppetion you can modify it
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
 echo 'you are connected';
+$q = "INSERT INTO `admin`.`items`(`id`,`name`) VALUES(NULL,'admin')";
+$db->exec($q);
 }
 catch(PDOException $e){
 echo 'Failed' .' '. $e->getMessage();
